f82ae2e3d45811be5ee593113b18242e1b5eef17
AndrewYang0618 <169140536+AndrewYang0618@users.noreply.github.com>
2024-06-19 10:47:36 +0800

    Add files via upload

--- 第一次作业.kt
+++ 第一次作业.kt
@@ -1,36 +1,39 @@
-package com.example.myapplication
+package com.example.greetingcard_1
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.ui.unit.sp
+import com.example.greetingcard_1.ui.theme.GreetingCard1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            GreetingCard1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RGBDemo()
+                    GreetingImage(message = "Happy Birthday Sams", from = "From XXX")
                 }
             }
         }
@@ -38,39 +41,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Hello(modifier: Modifier = Modifier) {
-    Text(
-        text = "2207020624+杨博锐",
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
+    Column(
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
-}
-@Composable
-fun RGBDemo(modifier: Modifier = Modifier) {
-    Row {
-        Box (
-            modifier = modifier.width(133.dp).fillMaxHeight().background(Color.Red),
-            contentAlignment = Alignment.Center
-        ){
-            Text(text = "红色");
-        }
-        Box (
-            modifier = modifier.width(133.dp).fillMaxHeight().background(Color.Green),
-            contentAlignment = Alignment.Center
-        ){
-            Text(text = "绿色");
-        }
-        Box (
-            modifier = modifier.width(133.dp).fillMaxHeight().background(Color.Blue),
-            contentAlignment = Alignment.Center
-        ){
-            Text(text = "蓝色");
-        }
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
     }
 }
-@Preview(showBackground = true)
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
+    val image = painterResource(id = R.drawable.androidparty)
+    Box(Modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+
 @Composable
 fun GreetingPreview() {
-    MyApplicationTheme {
-        RGBDemo()
+    GreetingCard1Theme {
+        //GreetingText(message = "Happy Birthday Sam!", from = "From Emma")
+        GreetingImage(message = "Happy Birthday Samsssssss", from = "From XXX")
     }
 }
